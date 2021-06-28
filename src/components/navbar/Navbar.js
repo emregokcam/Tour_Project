a73129a05692cacd6ea96705906f2fa3d6548d99
E2361Emre <67741583+E2361Emre@users.noreply.github.com>
2021-06-28 22:06:18 +0300

    ADDED: Components folder and Navbar.js

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <a href="aboutus">ABOUT US</a>
+      <a href="foryou">FOR YOU</a>
+      <a href="services">SERVICES</a>
+      <a href="blog">BLOG</a>
+      <a href="vlog">VLOG</a>
+      <a href="contact">CONTACT</a>
+    </div>
+  );
+}
+
+export default Navbar;
