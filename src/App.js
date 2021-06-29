830618580422e7985ee2911c0f530e4f781c2056
E2361Emre <67741583+E2361Emre@users.noreply.github.com>
2021-06-29 23:11:16 +0300

    Project Completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Header from "./components/header/Header";
 import Card from "./components/cards/Card";
+import "./App.css";
 
 function App() {
   return (
